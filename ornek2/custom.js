da67137cb6c95e6e1dade517edf81a5abeed5d03
aytugkerim <aytugkerim.sahin@gmail.com>
2020-02-14 23:22:49 +0300

    Initial Commit

---
+++ ornek2/custom.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function () {
+
+                window.location.hash = hash;
+            });
+        }
+    });
+})
+
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 650) {
+      $(this).addClass("slide");
+    }
+  });
+});
